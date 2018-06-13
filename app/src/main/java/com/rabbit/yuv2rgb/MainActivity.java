9394b3249cef1b14890fa59b7141ed2a0790c5a4
Even <EvenComeOn@qq.com>
2018-06-13 14:48:57 +0800

    删除无用代码

--- app/src/main/java/com/rabbit/yuv2rgb/MainActivity.java
+++ app/src/main/java/com/rabbit/yuv2rgb/MainActivity.java
@@ -27,15 +27,4 @@ public class MainActivity extends Activity {
             }
         });
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-       // System.loadLibrary("native-lib");
-    }
 }
